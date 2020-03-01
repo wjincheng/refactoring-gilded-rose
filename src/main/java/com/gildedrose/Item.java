25b919b87a961863b7cb8b11dad1b6ec7decaefa
“wjincheng” <2411465039@qq.com>
2020-03-01 13:06:58 +0800

    move update quality to Item class

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -8,14 +8,65 @@ public class Item {
 
     public int quality;
 
-    public Item(String name, int sell_in, int quality) {
+    public Item(String name, int sell_in, int quality){
         this.name = name;
         this.sell_in = sell_in;
         this.quality = quality;
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         return this.name + ", " + this.sell_in + ", " + this.quality;
+    }
+
+    public void update_quality(){
+        if (!name.equals("Aged Brie")
+            && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sell_in < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sell_in < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sell_in = sell_in - 1;
+        }
+
+        if (sell_in < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
     }
 }
---
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -0,0 +1,122 @@
+package com.gildedrose;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTest {
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_general_goods_and_sell_greater_than_0() {
+        Item item = new Item("foo", 1, 5);
+        item.update_quality();
+        assertEquals("foo", item.name);
+        assertThat(item.quality, is(4));
+        assertThat(item.sell_in, is(0));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_general_goods_and_sell_less_than_0() {
+        Item item = new Item("foo", -2, 6);
+       item.update_quality();
+        assertEquals("foo", item.name);
+        assertThat(item.quality, is(4));
+        assertThat(item.sell_in, is(-3));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Aged_Brie_and_sell_greater_than_0() {
+        Item item = new Item("Aged Brie", 1, 5);
+       item.update_quality();
+        assertEquals("Aged Brie", item.name);
+        assertThat(item.quality, is(6));
+        assertThat(item.sell_in, is(0));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Aged_Brie_and_sell_less_than_0() {
+        Item item = new Item("Aged Brie", -2, 6);
+       item.update_quality();
+        assertEquals("Aged Brie", item.name);
+        assertThat(item.quality, is(8));
+        assertThat(item.sell_in, is(-3));
+    }
+
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Aged_Brie_and_quality_greater_than_50() {
+        Item item = new Item("Aged Brie", -1, 55);
+       item.update_quality();
+        assertEquals("Aged Brie", item.name);
+        assertThat(item.quality, is(55));
+        assertThat(item.sell_in, is(-2));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Sulfuras_and_sell_greater_than_0() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 1, 5);
+       item.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", item.name);
+        assertThat(item.quality, is(5));
+        assertThat(item.sell_in, is(1));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Sulfuras_and_sell_less_than_0() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", -2, 6);
+       item.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", item.name);
+        assertThat(item.quality, is(6));
+        assertThat(item.sell_in, is(-2));
+    }
+
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Sulfuras_and_quality_greater_than_50() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", -1, 55);
+       item.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", item.name);
+        assertThat(item.quality, is(55));
+        assertThat(item.sell_in, is(-1));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Sulfuras_and_quality_greater_than_50_and_sell_is_0() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 55);
+       item.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", item.name);
+        assertThat(item.quality, is(55));
+        assertThat(item.sell_in, is(0));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_TAFKAL80ETC_and_sell_greater_than_0() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 5);
+       item.update_quality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", item.name);
+        assertThat(item.quality, is(8));
+        assertThat(item.sell_in, is(0));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_TAFKAL80ETC_and_sell_less_than_0() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", -2, 6);
+       item.update_quality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", item.name);
+        assertThat(item.quality, is(0));
+        assertThat(item.sell_in, is(-3));
+    }
+
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_TAFKAL80ETC_and_quality_greater_than_50() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 55);
+       item.update_quality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", item.name);
+        assertThat(item.quality, is(55));
+        assertThat(item.sell_in, is(0));
+    }
+
+
+}
