ab7ce5ad6cccb5446071393aca1fcba9eb7e984d
“wjincheng” <2411465039@qq.com>
2020-03-01 12:08:13 +0800

    edit test for Aged Brie

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -50,12 +50,12 @@ public class GildedRoseTest {
 
     @Test
     public void should_sell_minus_1_and_quality_plus_1_given_Aged_Brie_and_quality_greater_than_50() {
-        Item[] items = new Item[] { new Item("Aged Brie", 1, 55) };
+        Item[] items = new Item[] { new Item("Aged Brie", -1, 55) };
         GildedRose app = new GildedRose(items);
         app.update_quality();
         assertEquals("Aged Brie", app.items[0].name);
         assertThat(app.items[0].quality, is(55));
-        assertThat(app.items[0].sell_in, is(0));
+        assertThat(app.items[0].sell_in, is(-2));
     }
 
 }
