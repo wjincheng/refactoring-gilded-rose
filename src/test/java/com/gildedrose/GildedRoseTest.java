782ce24a53bdae584ec84165fe8b695abca585e9
“wjincheng” <2411465039@qq.com>
2020-03-01 12:30:28 +0800

    add test for Sulfuras, Hand of Ragnaros when sell is 0

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -90,6 +90,16 @@ public class GildedRoseTest {
     }
 
     @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_Sulfuras_and_quality_greater_than_50_and_sell_is_0() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 55) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertThat(app.items[0].quality, is(55));
+        assertThat(app.items[0].sell_in, is(0));
+    }
+
+    @Test
     public void should_sell_minus_1_and_quality_plus_1_given_TAFKAL80ETC_and_sell_greater_than_0() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 1, 5) };
         GildedRose app = new GildedRose(items);
