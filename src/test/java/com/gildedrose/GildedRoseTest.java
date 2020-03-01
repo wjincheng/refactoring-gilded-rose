8342da19da76c2ab5d5703c03e12b5a685948563
“wjincheng” <2411465039@qq.com>
2020-03-01 11:51:02 +0800

    add test for general goods

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,13 +8,23 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void should_sell_minus_1_and_quality_plus_1_given_general_goods_and_sell_greater_than_0() {
         Item[] items = new Item[] { new Item("foo", 1, 5) };
         GildedRose app = new GildedRose(items);
         app.update_quality();
         assertEquals("foo", app.items[0].name);
         assertThat(app.items[0].quality, is(4));
         assertThat(app.items[0].sell_in, is(0));
+    }
+
+    @Test
+    public void should_sell_minus_1_and_quality_plus_1_given_general_goods_and_sell_less_than_0() {
+        Item[] items = new Item[] { new Item("foo", -2, 6) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertEquals("foo", app.items[0].name);
+        assertThat(app.items[0].quality, is(4));
+        assertThat(app.items[0].sell_in, is(-3));
     }
 
 }
