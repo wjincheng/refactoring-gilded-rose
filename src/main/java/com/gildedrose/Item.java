35b4eb392d7674a64b015e683603b86d6d82c667
“wjincheng” <2411465039@qq.com>
2020-03-01 15:02:32 +0800

    switch to replace if

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -32,11 +32,22 @@ public class Item {
     }
 
     public void update_quality(){
-        if (name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (quality < 50) {
-                quality = quality + 1;
+        switch (name) {
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
 
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (sellIn < 11) {
                         if (quality < 50) {
                             quality = quality + 1;
@@ -49,31 +60,25 @@ public class Item {
                         }
                     }
                 }
-            }
-        } else {
-            if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if (quality > 0) {
                     quality = quality - 1;
                 }
-            }
-        }
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
 
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sellIn = sellIn - 1;
-        }
-
-        if (sellIn < 0) {
-            if (name.equals("Aged Brie")) {
-                if (quality < 50) {
-                    quality = quality + 1;
                 }
-            } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                quality = 0;
-            } else {
-                if (quality > 0 && !name.equals("Sulfuras, Hand of Ragnaros")) {
-                    quality = quality - 1;
-                }
-            }
+                break;
         }
     }
 }
---
+++ src/main/java/com/gildedrose/ItemImpl.java
@@ -0,0 +1,6 @@
+package com.gildedrose;
+
+public interface ItemImpl {
+
+    public void updateQuality();
+}
